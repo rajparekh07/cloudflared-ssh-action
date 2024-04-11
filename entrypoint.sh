b7fbf501a2ebd813334d845123059fda4ad2fbdd
Raj Parekh <raj.parekh07@gmail.com>
2024-04-11 08:15:06 +0300

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,12 +1,21 @@
 #!/bin/sh -l
 
+echo "Preparing Config"
+
 echo "Host $1" >> /root/.ssh/config
 echo "ProxyCommand cloudflared access ssh --hostname %h" >> /root/.ssh/config
+
+
+echo "Preparing Key"
+
 echo "$4" > /root/.ssh/key.pem
 chmod 600 /root/.ssh/key.pem
 
+echo "Preparing SSH"
+
 ssh-keyscan $1 >> /root/.ssh/known_hosts
 
-ssh -o StrictHostKeyChecking=no $3@$1
+# ssh -o StrictHostKeyChecking=no $3@$1
 
+echo "SSHing begins"
 ssh -i /root/.ssh/key.pem $3@$1 -p $2 "$5"
