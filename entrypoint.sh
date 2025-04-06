140066c36da93660e5df40a3abbcf5caf9109762
Raj Parekh <raj.parekh07@gmail.com>
2025-04-06 12:30:33 +0300

    debug more

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,29 +1,27 @@
 #!/bin/sh -l
+
+# Set CF Access token to ENV so cloudflared can use it automatically
 export CF_ACCESS_CLIENT_ID="$7"
 export CF_ACCESS_CLIENT_SECRET="$8"
 
 # Add Host to SSH config
 echo "Host $1" >> /root/.ssh/config
 
-# Add ProxyCommand depending on token presence
-if [ -z "$7" ] || [ -z "$8" ]; then
-    echo "ProxyCommand cloudflared access ssh --hostname %h" >> /root/.ssh/config
-else
-    echo "ProxyCommand cloudflared access ssh --hostname %h --id $7 --secret $8" >> /root/.ssh/config
-fi
+# Add ProxyCommand (no --id/--secret since env is used)
+echo "ProxyCommand cloudflared access ssh --hostname %h" >> /root/.ssh/config
 
-# Set user
+# Set SSH user
 echo "User $3" >> /root/.ssh/config
 
 # Save the private key
 echo "$5" > /root/.ssh/$4
 chmod 600 /root/.ssh/$4
 
-# Scan and add host key
+# Add host to known_hosts
 ssh-keyscan "$1" >> /root/.ssh/known_hosts
 
-# Debug: show final config
+# Debug output
 cat /root/.ssh/config
 
-# SSH command execution
+# Execute SSH command using the config
 ssh -i /root/.ssh/$4 -p "$2" -o StrictHostKeyChecking=no -F /root/.ssh/config "$3@$1" -v "$6"
